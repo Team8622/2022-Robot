a987f1dfefbef26181cbdb55ea0d6be36afc0e0d
RachelMo8068 <52724995+RachelMo8068@users.noreply.github.com>
2022-01-30 16:57:37 -0500

    idk man I'm updating things

---
+++ src/main/java/frc/robot/misc/ControlChooser.java
@@ -0,0 +1,18 @@
+package frc.robot.misc;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+import frc.robot.RobotContainer;
+//import commands to be connected to joystick buttons
+
+public class ControlChooser {
+    public static Joystick leftJoy = new Joystick(0);
+    public static Joystick rightJoy = new Joystick(1);
+
+    //Define buttons here
+    //Button exampleButton = new JoystickButton(exampleJoystick, index).whenPressed(new command());
+
+    //if we want to configure this for different drivers
+}
