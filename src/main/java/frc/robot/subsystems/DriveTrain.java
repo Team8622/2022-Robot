0af47fb96b97c3fa9f4ebf68e4c6dd7611503697
RachelMo8068 <52724995+RachelMo8068@users.noreply.github.com>
2022-01-30 07:00:25 -0500

    fixed an import statement

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -13,7 +13,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.MotorType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
