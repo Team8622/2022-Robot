0dde59fd01e644ebc699f6bc5b78fb3287a18fc9
RachelMo8068 <52724995+RachelMo8068@users.noreply.github.com>
2022-01-29 19:01:08 -0500

    updated code to 2022 version

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,19 @@
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+
+package frc.robot;
+
+public class Constants {
+    public static final class CANBusID{
+        public static final int driveLeft1 = 1;
+        public static final int driveLeft2 = 2;
+        public static final int driveRight1 = 3;
+        public static final int driveRight2 = 4;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+/**
+ * This is the class definition for the DriveTrain object
+ */
+
+/** */
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.CANBusID;
+
+public class DriveTrain extends SubsystemBase{
+  
+  //Left Side Motor Controllers
+  private final CANSparkMax m_leftlead = new CANSparkMax(CANBusID.driveLeft1, MotorType.kBrushless);  
+  private final CANSparkMax m_leftfollow = new CANSparkMax(CANBusID.driveLeft2, MotorType.kBrushless);
+  
+  //Right Side Motor Controllers
+  private final CANSparkMax m_rightlead = new CANSparkMax(CANBusID.driveRight1, MotorType.kBrushless);
+  private final CANSparkMax m_rightfollow = new CANSparkMax(CANBusID.driveRight2, MotorType.kBrushless);
+
+  DifferentialDrive m_drive = new DifferentialDrive(m_leftlead, m_rightlead);
+
+
+  public void TankDrive(double left, double right){
+      m_drive.tankDrive(left, right);
+  }
+
+  public void ArcadeDrive(double xSpeed, double zRotation){
+      m_drive.arcadeDrive(xSpeed, zRotation);
+  }
+}
