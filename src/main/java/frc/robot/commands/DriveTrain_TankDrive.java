6d9b88562b6d02bd779e17ee87184d1ddc9bc075
RachelMo8068 <52724995+RachelMo8068@users.noreply.github.com>
2022-02-08 10:47:50 -0500

    imported because nat said so

---
+++ src/main/java/frc/robot/commands/DriveTrain_TankDrive.java
@@ -0,0 +1,48 @@
+package frc.robot.commands;
+
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.RobotContainer;
+import frc.robot.misc.ControlChooser;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.Constants;
+
+public class DriveTrain_TankDrive extends CommandBase{
+    //this looks useful but I literally do not think its used anywhere else
+    //double driveSpeed = 10; //maximum percentage of motor to use
+    
+    /**
+     * Creates a new DriveTrain_TankDrive
+     */
+    public DriveTrain_TankDrive(DriveTrain subsystem){
+        addRequirements(RobotContainer.m_driveTrain);
+    }
+
+    //called when the command is initially scheduled
+    @Override
+    public void initialize() {
+        //driveSpeed = SmartDashboard.getNumber("Speed Percentage", 100);
+    }
+
+    //called when the scheduler runs while the command is scheduled
+    @Override
+    public void execute() {
+        RobotContainer.m_driveTrain.TankDrive(
+            -ControlChooser.stick.getRawAxis(Constants.CANBusID.leftYAxis),
+            -ControlChooser.stick.getRawAxis(Constants.CANBusID.rightYAxis)
+        );
+    }
+
+    //called when the command ends or is interrupted
+    @Override
+    public void end(boolean interrupted){
+    } 
+
+    //returns true when command should end
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+}
