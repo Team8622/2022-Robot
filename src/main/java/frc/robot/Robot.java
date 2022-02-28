a879d9e885deb2c6f79133ec9e97bba5eb45df45
RachelMo8068 <52724995+RachelMo8068@users.noreply.github.com>
2022-02-28 18:09:43 -0500

    idk something's wrong

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -118,7 +119,12 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+  //Nathan -- Test arcade drive code
+  
+
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
