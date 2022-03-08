b9487f03498e105b8de5549f20dfdf3197fe279d
RachelMo8068 <52724995+RachelMo8068@users.noreply.github.com>
2022-03-08 02:24:19 -0500

    I think I finally found the winch error - .set()

--- src/main/java/frc/robot/subsystems/Winch.java
+++ src/main/java/frc/robot/subsystems/Winch.java
@@ -1,80 +1,55 @@
+/**
+ * @author Rachel Mohammed
+ * March 8th, 2022
+ */
+
 package frc.robot.subsystems;
 
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-
+import frc.robot.Constants.CANBusID;
 
 public class Winch extends SubsystemBase{
+  
+  //motor controllers
+  private CANSparkMax m_lead;
+  private CANSparkMax m_follow;
+  public double winchspeed;
+  public boolean iswinchon = false;
 
-private CANSparkMax m_L_armLead;
-private CANSparkMax m_R_armFollow;
-private RelativeEncoder m_encoder;
-public double winchspeed;
-public void init(){
-    try{
-        m_L_armLead = new CANSparkMax(Constants.CANBusID.armLead, MotorType.kBrushless);
-        m_R_armFollow = new CANSparkMax(Constants.CANBusID.armFollow, MotorType.kBrushless);
-        m_L_armLead.restoreFactoryDefaults();
-        m_R_armFollow.restoreFactoryDefaults();
-        m_R_armFollow.follow(m_L_armLead);
-        m_encoder = m_L_armLead.getEncoder();
+  public void init(){
+    m_lead = new CANSparkMax(CANBusID.armLead, MotorType.kBrushless);
+    m_follow = new CANSparkMax(CANBusID.armFollow, MotorType.kBrushless);
 
-        SmartDashboard.putBoolean("Winch Init", true);
-        } catch (RuntimeException ex){
-            DriverStation.reportError("error loading failed" + ex.getMessage(), true);
-        } 
-      winchspeed = Constants.SpeedConstants.winchmotorspeed;
-        
-    
+    m_follow.follow(m_lead);
+    winchspeed = Constants.SpeedConstants.winchmotorspeed;
   }
 
   @Override
-  public void periodic() {
-    // double velocity_out;
-    winchspeed = SmartDashboard.getNumber("Winch Velocity", 0);
-    
-    double velocity_out;
-    try {
-      velocity_out = m_encoder.getVelocity();
-    } catch (RuntimeException ex){
-      DriverStation.reportError("Winch: Not able to get velocity " + ex.getMessage(),true);
-      velocity_out = 0;
-    }
-    SmartDashboard.putNumber("Winch motor Velocity",velocity_out);
-    SmartDashboard.putNumber("Output", m_L_armLead.getAppliedOutput());
+  public void periodic(){
+    SmartDashboard.putNumber("Winch Output", m_lead.getAppliedOutput());
   }
 
- public boolean iswinchon = false;
- public void winchon(){
-   
-    m_L_armLead.set(winchspeed);
-   //m_pidController.setReference(-winchspeed, ControlType.kVelocity);
-   //m_R_armFollow.follow(m_L_armLead);
+  public void winchon(){
+    m_lead.set(0.75);
+    SmartDashboard.putBoolean("Winch On", true);
+    iswinchon = true;
+  }
 
-   SmartDashboard.putBoolean("Winch On", true);
-   iswinchon = true;
- }
- public void reverseon(){
-  m_L_armLead.set(-winchspeed * 0.25);
-  //m_R_armFollow.follow(m_L_armLead);
+  public void winchreverse(){
+    m_lead.set(-0.25);
+    SmartDashboard.putBoolean("Winch On", true);
+    iswinchon = true;
+  }
 
-  SmartDashboard.putBoolean("Winch On", true);
-  iswinchon = true;
-}
- public void winchoff(){
-   
-  //m_pidController.setReference(winchspeed, ControlType.kVelocity);
-  m_L_armLead.set(0);
-  m_R_armFollow.set(0);
-
-  SmartDashboard.putBoolean("Winch On", false);
-  iswinchon = false;
- }
+  public void winchoff(){
+    m_lead.set(0);
+    SmartDashboard.putBoolean("Winch On", false);
+    iswinchon = false;
+  }
 }
 
